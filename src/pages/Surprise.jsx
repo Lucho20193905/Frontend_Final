2ca026c666815117d2cfaba15f4808eb0eb17914
Luis <20193905@aloe.ulima.edu.pe>
2022-11-24 16:05:28 -0500

    a

--- src/pages/Surprise.jsx
+++ src/pages/Surprise.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import {  } from "react-router-dom";
-import "../pages/styles";
+
 
 const Surprise = () => {
   return <div>
