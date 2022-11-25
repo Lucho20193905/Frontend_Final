24d7083417c1ab9767d6be01854654f5774fccb1
Luis <20193905@aloe.ulima.edu.pe>
2022-11-24 20:07:39 -0500

    a

--- src/pages/luis/carrito/Carrito.jsx
+++ src/pages/luis/carrito/Carrito.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from "react"
-import {  } from "react-router-dom";
 import { useNavigate } from "react-router"
-import Header from "../../components/Header"
+import Header from "../../../components/Header"
+
+const Carrito = () =>{
+  const [listadoOrdenProducto, setListadoOrdenProducto] = useState([])
+}
+
 
 
 
